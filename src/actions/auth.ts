df0bfcfcd508b904d5655829360570d03f4748d6
Jatin <183578648+jatin02k@users.noreply.github.com>
2026-07-09 19:50:09 +0530

    add auth server action

---
+++ src/actions/auth.ts
@@ -0,0 +1,33 @@
+'use server'
+
+import { createClient } from "@/app/lib/supabase/server"
+import { redirect } from "next/dist/client/components/navigation"
+
+export async function sentOtp(email:string){
+    const supabase = await createClient()
+    const { data, error } = await supabase.auth.signInWithOtp({
+        email,
+        options:{
+            shouldCreateUser: true
+        }
+    })
+
+    if (error){
+        return { success: false, error: error.message }
+    }
+    return { success: true, data: data }
+}
+
+export async function verifyOtp(email:string, token:string){
+    const supabase = await createClient()
+    const { data, error } = await supabase.auth.verifyOtp({
+        email,
+        token,
+        type: 'email'
+    })
+
+    if (error) {
+        return {success: false, error: error.message}
+    }
+    redirect('/dashboard')
+}
